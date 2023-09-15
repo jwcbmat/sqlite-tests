82d8e4d032901ef3caf35d33828bdfe77cf5b1c0
jwcbmat <johnathan.wcb@gmail.com>
2023-09-14 22:45:50 -0300

    feat: crud sqlite :o

--- src/app.ts
+++ src/app.ts
@@ -6,40 +6,13 @@ import { Sqlite } from "./sqlite/sqlite.js"
 // TODO: construir emissor de evento para logar erros. *talvez*
 // TODO: fazer documentação de funções e classes
 // TODO: ponto de atenção para interfaces com métodos estáticos
-// TODO: deletePerson(): Promise<void> {}
-// TODO: deleteAllPersons()
-// TODO: async selectPerson(): Promise<void> {}
-// TODO: selectAllPersons()
 
+console.log("PROCESS: starting service")
 /**
 	* Initial service
  * **/
-function initialApp(): Promise<void | Error> {
-	return new Promise(async (resolve, reject) => {
-		try {
-			Sqlite.getInstance()
-			Sqlite.createTable()
-
-			Sqlite.insertData("juao1")
-			console.log("DONE: insert data success")
-
-		} catch (e) {
-			reject(e)
-		}
-
-		// create database
-		// create table person
-		// insert data to person table
-	})
+function initialApp(): void {
+	Sqlite.getInstance()
 }
 
 initialApp()
-	.then(response => {
-		console.log("DONE: ", response)
-	})
-	.catch(err => {
-		console.error("ERROR: ", err)
-	})
-	.finally(() => {
-		Sqlite.closeDatabase()
-	})
--- src/sqlite/sqlite.ts
+++ src/sqlite/sqlite.ts
@@ -1,54 +1,137 @@
-import { Isqlite } from "./sqlite.interface.js"
 import * as sqlite3 from "sqlite3"
 import { Database } from "sqlite3"
 
-class Sqlite implements Isqlite  {
-	private static db: sqlite3.Database
+type Person = { id: number, name: string }
 
-	public static getInstance(): Database {
-		if(!Sqlite.db) {
-			Sqlite.db = new sqlite3.Database(":memory:", (err) => {
-				if (err) return console.error(err.message)
+class Sqlite  {
+	private db!: sqlite3.Database
+	private static instance: Sqlite | null = null
+
+	private constructor() {
+		console.log("PROCESS: Starting Sqlite in-memory")
+		this.init()
+	}
+
+	public static getInstance(): Sqlite {
+		if(!Sqlite.instance) Sqlite.instance = new Sqlite()
+
+		return Sqlite.instance
+	}
+
+	private init(): Database {
+		if(!this.db) {
+			this.db = new sqlite3.Database(":memory:", (err) => {
+				if (err) {
+					console.error(err.message)
+					throw new Error(err.message)
+				}
+				
 				console.log("DONE: Connected to the in-memory SQlite database.")
 			})
 		}
-
-		return Sqlite.db
+		Sqlite.createTable()
+		
+		return this.db
 	}
 
 	public static closeDatabase(): void {
-		Sqlite.db.close((err) => {
-			if (err) console.error(err.message)
-			console.log("DONE: Close the database connection.")
+		this.getInstance().db.close((err) => {
+			if (err) {
+				console.error("Error: closeDatabase fail")
+				throw new Error(err.message)
+			}
+
+			console.log("DONE: closeDatabase success")
 		})
 	}
 
 	public static createTable(): void {
-		this.getInstance().exec("CREATE TABLE PERSON (" +
+		this.getInstance().db.exec("CREATE TABLE PERSON (" +
 			" id INTEGER PRIMARY KEY AUTOINCREMENT," +
 			" name TEXT NOT NULL" +
 			")",
 			(err) => {
 				if(err) {
-					console.error("ERROR: create table error")
+					console.error("ERROR: createTable error")
 					throw new Error(err.message)
 				}
+				
+				console.log("DONE: createTable success")
 			}
 		)
 	}
 
-	public static insertData(name: string): void {
-		this.getInstance().exec(`INSERT INTO PERSON ('name') values ('${name}')`,
+	public static insertPerson(name: string): void {
+		const INSERT_PERSON = `INSERT INTO PERSON ('name') values ('${name}')`
+
+		this.getInstance().db.exec(INSERT_PERSON,
 			(err) => {
 				if(err) {
 					console.error("ERROR: insert data error")
 					throw new Error(err.message)
 				}
+
+				console.log("DONE: insert data success")
 			}
 		)
 	}
-	async deleteData(): Promise<void> {}
-	async selectData(): Promise<void> {}
+
+	public static deletePerson(id: number): void {
+		const DELETE_PERSON = `DELETE FROM PERSON WHERE id = '${id}'`
+		
+		this.getInstance().db.run(DELETE_PERSON, (err) => {
+				if(err) {
+					console.log("ERROR: deletePerson error")
+					throw new Error(err.message)
+				}
+				
+				console.log("DONE: deletePersonSuccess")
+		})
+	}
+	
+	public static deleteAll(): void {
+		const DELETE_ALL = `DELETE FROM PERSON`
+		
+		this.getInstance().db.run(DELETE_ALL, (err) => {
+			if(err) {
+				console.log("ERROR: deleteAll error")
+				throw new Error(err.message)
+			}
+			
+			console.log("DONE: deleteAll")
+		})
+	}
+
+	public static selectPerson(name: string): Promise<string> {
+		const SELECT_PERSON = `SELECT id, name FROM PERSON WHERE name = '${name}'`
+		
+		return new Promise((resolve, reject) => {
+		 this.getInstance().db.get(
+				SELECT_PERSON, (err, row: Person) => {
+						if(err) {
+							console.log("ERROR: selectPerson error")
+							reject(new Error(err.message))
+					}
+					
+					resolve(row.name)
+			})
+		})
+	}
+
+	public static selectAll(): Promise<Person[]> {
+		const SELECT_ALL = `SELECT id, name FROM PERSON`
+
+		return new Promise((resolve, reject) => {
+			this.getInstance().db.all(SELECT_ALL, (err, row: Person[]) => {
+				if(err) {
+					console.error("ERROR: selectAll error")
+					reject(new Error(err.message))
+				}
+	
+				resolve(row)
+			})
+		})
+	}
 }
 
 export { Sqlite }
